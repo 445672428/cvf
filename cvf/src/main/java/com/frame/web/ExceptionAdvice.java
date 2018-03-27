c6761d5a548b4e36d89c154fc4a12c143847dc63
bobo <bobo@bobo-PC>
2018-03-27 11:38:28 +0800

    程占波

---
+++ cvf/src/main/java/com/frame/web/ExceptionAdvice.java
@@ -0,0 +1,58 @@
+package com.frame.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.base.BaseAction;
+import com.entities.ResultMeta;
+
+@ControllerAdvice  
+@ResponseBody
+public class ExceptionAdvice extends BaseAction{
+	
+	/** 
+     * 400 - Bad Request 
+     */  
+    @ResponseStatus(HttpStatus.BAD_REQUEST)  
+    @ExceptionHandler(HttpMessageNotReadableException.class)  
+    public ResultMeta handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {  
+        logger.error("参数解析失败", e);  
+        return new ResultMeta().failure("could_not_read_json");  
+    }  
+  
+    /** 
+     * 405 - Method Not Allowed 
+     */  
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)  
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)  
+    public ResultMeta handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {  
+        logger.error("不支持当前请求方法", e);  
+        return new ResultMeta().failure("request_method_not_supported");  
+    }  
+  
+    /** 
+     * 415 - Unsupported Media Type 
+     */  
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)  
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)  
+    public ResultMeta handleHttpMediaTypeNotSupportedException(Exception e) {  
+        logger.error("不支持当前媒体类型", e);  
+        return new ResultMeta().failure("content_type_not_supported");  
+    }  
+  
+    /** 
+     * 500 - Internal Server Error 
+     */  
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  
+    @ExceptionHandler(Exception.class)  
+    public ResultMeta handleException(Exception e) {  
+        logger.error("服务运行异常", e);  
+        return new ResultMeta().failure(e.getMessage());  
+    }  
+}
---
+++ cvf/src/main/java/com/mybatis/service/MyBaseService.java
@@ -0,0 +1,7 @@
+package com.mybatis.service;
+
+import java.io.Serializable;
+
+public class MyBaseService<T extends Serializable>{
+	
+}
